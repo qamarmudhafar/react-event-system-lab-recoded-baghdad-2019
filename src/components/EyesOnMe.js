9fae5f8f1bf3e5ec4adabb0fee17faa9256f61f1
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-14 17:07:40 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component{
+
+Focus=()=>{
+  console.log("Good!")
+}
+Blur=()=>{
+  console.log("Hey! Eyes on me!")
+}
+  render(){
+    return(
+      <button onFocus={this.Focus} onBlur={this.Blur}></button>
+    )
+  }
+}
